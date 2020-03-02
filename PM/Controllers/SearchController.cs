463604a9bd036f1730d50885040bc4703f82355f
DrastamatSargsyan <drastamatsargsyan@gmail.com>
2020-03-02 14:33:17 +0400

    Main Search

---
+++ PM/Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PRODUCT_MANAGEMENT.DataAccess;
+using PRODUCT_MANAGEMENT.Models;
+
+namespace PRODUCT_MANAGEMENT.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    [EnableCors("AllowAllHeaders")]
+    public class SearchController : ControllerBase
+    {
+        private readonly DataAccessSearch Data = new DataAccessSearch();
+
+        [HttpPost("{product}")]
+        public IActionResult Search (string product)
+        {
+            try
+            {
+                var obj = JsonConvert.DeserializeObject<ProductsInBW>(product);
+
+                var result = Data.Search(obj).ToList();
+
+                if(result.Count()==0)
+                {
+                    return Ok(false);
+                }
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ PM/Models/ProductsInBW.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRODUCT_MANAGEMENT.Models
+{
+    public class ProductsInBW
+    {
+        public string Barcode { get; set; }
+        public decimal Cost_Price { get; set; }
+        public decimal Selling_Price { get; set; }
+        public string Supplier_Name { get; set; }
+        public string Product_Name { get; set; }
+        public string Category_Name { get; set; }
+        public string BW_Name { get; set; }
+        public int Count { get; set; }
+    }
+}
